d423f56ddfa4f454eec779e417fe1a1b771183ea
PushkarKumarPandey005 <pushkarkumar121005@gmail.com>
2026-02-10 03:43:08 +0530

    final cors fix

--- src/app.js
+++ src/app.js
@@ -1,32 +1,27 @@
 import express from 'express';
-import cors from 'cors';
 import productsRoutes from './routes/product.router.js';
 import userRoutes from "./routes/user.router.js";
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL,
-];
-
+// ---- CORS (robust for preflight) ----
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Vary", "Origin");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
   if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // <-- THIS FIXES PREFLIGHT
+    return res.sendStatus(200);
   }
 
   next();
 });
+// -------------------------------------
 
 app.use(express.json());
 
